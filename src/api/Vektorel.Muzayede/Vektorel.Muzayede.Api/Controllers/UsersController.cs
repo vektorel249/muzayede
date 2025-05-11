314bbb99982034615c32a7cbe49388c74845c60b
GurkanYildizz <gurkan_yildiz@outlook.com.tr>
2025-05-11 12:07:11 +0300

    user kontroller eklendi

---
+++ src/api/Vektorel.Muzayede/Vektorel.Muzayede.Api/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vektorel.Muzayede.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public UsersController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        await Task.CompletedTask;
+        return Ok("liste boş");
+    }
+}
