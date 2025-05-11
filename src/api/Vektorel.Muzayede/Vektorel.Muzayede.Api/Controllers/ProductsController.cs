21b2238f58ce18c2d65814f22a4cb90236b44379
Muco0652 <ordulumuco52@gmail.com>
2025-05-11 12:03:32 +0300

    Product controller eklendi

---
+++ src/api/Vektorel.Muzayede/Vektorel.Muzayede.Api/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Vektorel.Muzayede.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public ProductsController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    public async Task<IActionResult> GetProducts()
+    {
+        return Ok("ürün listesi boş");
+    }
+}
